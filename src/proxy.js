c4c251f186b787f44158af78cf5ab8dcdaf13cb1
Arnob-450 <tawhidkhanarnob@gmail.com>
2026-09-15 17:22:36 +0600

    proxy is added and login is fully functional

---
+++ src/proxy.js
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+
+
+export async function proxy (request) {
+    
+     const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    // console.log(session,'session');
+    // console.log("hello");
+
+    const isLoggedIN = false;
+    if (session) {
+        return NextResponse.next();
+    }
+    else{
+      
+    return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+
+
+
+
+}
+
+
+export const config = {
+    matcher: ["/career","/news/:path*"],
+}
